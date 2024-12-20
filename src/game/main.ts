91ce1f894b4f5cfc22f12b40deddeaa8f5f636c0
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2024-12-20 22:00:18 +0700

    finish

--- src/game/main.ts
+++ src/game/main.ts
@@ -28,8 +28,8 @@ const config: Phaser.Types.Core.GameConfig = {
         default: 'arcade',
         arcade: {
             gravity: { x: 0, y: 0 },
-            debug: true,
-            debugShowVelocity: false
+            // debug: true,
+            // debugShowVelocity: false
         }
     },
     scene: [
